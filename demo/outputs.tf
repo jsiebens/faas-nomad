57f24836616fb01a007670aa8a55b4f649e677f0
Johan Siebens <johan.siebens@gmail.com>
2021-07-07 17:05:41 +0200

    update summary output
    Signed-off-by: Johan Siebens <johan.siebens@gmail.com>

--- demo/outputs.tf
+++ demo/outputs.tf
@@ -1,30 +1,22 @@
-output "nomad_addr" {
-  value = "http://${digitalocean_droplet.server.ipv4_address}:4646"
-}
+output "summary" {
+  value = <<CONFIGURATION
 
-output "consul_addr" {
-  value = "http://${digitalocean_droplet.server.ipv4_address}:8500"
-}
+The Nomad UI can be accessed at http://${digitalocean_droplet.server.ipv4_address}:4646/ui
+The Consul UI can be accessed at http://${digitalocean_droplet.server.ipv4_address}:8500/ui
+The Vault UI can be accessed at http://${digitalocean_droplet.server.ipv4_address}:8200/ui
+The OpenFaaS UI can be accessed at http://${digitalocean_loadbalancer.public.ip}/ui
 
-output "vault_addr" {
-  value = "http://${digitalocean_droplet.server.ipv4_address}:8200"
-}
+CLI environment variables:
 
-output "gateway" {
-  value = "http://${digitalocean_loadbalancer.public.ip}"
-}
-
-output "get_vault_token" {
-  value = "ssh root@${digitalocean_droplet.server.ipv4_address} \"grep Initial /etc/vault.d/vault-keys.log | cut -c21-\""
-}
-
-output "getting_started" {
-  value = <<EOF
 export CONSUL_HTTP_ADDR=http://${digitalocean_droplet.server.ipv4_address}:8500
 export NOMAD_ADDR=http://${digitalocean_droplet.server.ipv4_address}:4646
 export VAULT_ADDR=http://${digitalocean_droplet.server.ipv4_address}:8200
-export VAULT_TOKEN=$(ssh root@${digitalocean_droplet.server.ipv4_address} "grep Initial /etc/vault.d/vault-keys.log | cut -c21-")
 export OPENFAAS_URL=http://${digitalocean_loadbalancer.public.ip}
+export VAULT_TOKEN=$(ssh root@${digitalocean_droplet.server.ipv4_address} "grep Initial /etc/vault.d/vault-keys.log | cut -c21-")
+
+Authenticate with faas-cli:
+
 vault kv get -field=value openfaas/basic-auth-password | faas-cli login -u admin --password-stdin
-EOF
+
+CONFIGURATION
 }
